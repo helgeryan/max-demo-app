56ad48ac84d0ddb26c1ab266f29e5351ea350831
Ryan Helgeson <ryanhelgeson14@gmail.com>
2024-01-18 12:22:04 -0500

    Initial Commit

---
+++ maxdemo/maxdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  maxdemoApp.swift
+//  maxdemo
+//
+//  Created by Ryan Helgeson on 1/18/24.
+//
+
+import SwiftUI
+
+@main
+struct maxdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
