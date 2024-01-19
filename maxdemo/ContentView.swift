b519f5db656644d7d80c482a40c78e43eff5b1c6
Ryan Helgeson <ryanhelgeson14@gmail.com>
2024-01-18 20:09:27 -0500

    Create a tab view

--- maxdemo/ContentView.swift
+++ maxdemo/ContentView.swift
@@ -7,11 +7,60 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MainTabView: View {
+    @State var selectedTabIndex: Int = 0
+    var body: some View {
+        TabView(selection: $selectedTabIndex) {
+            HomeView()
+                .tabItem({
+                    VStack {
+                        Spacer()
+                        Image(systemName: "house")
+                        Spacer()
+                    }
+                })
+                .tag(0)
+            
+            HomeView()
+                .tabItem({
+                    VStack {
+                        Spacer()
+                        Image(systemName: "bookmark")
+                        Spacer()
+                    }
+                })
+                .tag(1)
+            
+            HomeView()
+                .tabItem({
+                    VStack {
+                        Spacer()
+                        Image(systemName: "arrow.down.to.line")
+                        Spacer()
+                    }
+                })
+                .tag(1)
+            
+            HomeView()
+                .tabItem({
+                    VStack {
+                        Spacer()
+                        Image(systemName: "magnifyingglass")
+                        Spacer()
+                    }
+                })
+                .tag(1)
+        }
+        .tint(.white)
+    }
+}
+
+
+struct HomeView: View {
     @EnvironmentObject var mediaManager: MediaManager
     @State var barOpacity: CGFloat = 0
-    
     var body: some View {
+        
         NavigationStack {
             ZStack {
                 ScrollView {
@@ -68,6 +117,27 @@ struct ContentView: View {
                 }
             })
         }
+    }
+}
+
+struct ContentView: View {
+    
+    init() {
+        UITabBar.appearance().unselectedItemTintColor = UIColor(Color.gray)
+        let appearance = UITabBarAppearance()
+        appearance.backgroundColor = UIColor(Color("tabbarcolor"))
+        UITabBar.appearance().standardAppearance = appearance
+        UITabBar.appearance().scrollEdgeAppearance = appearance
+        
+        UINavigationBar.appearance().barTintColor = .clear
+        let navAppearance = UINavigationBarAppearance()
+        navAppearance.backgroundColor = UIColor(Color.white)
+        UINavigationBar.appearance().standardAppearance = navAppearance
+        UINavigationBar.appearance().compactScrollEdgeAppearance = navAppearance
+    }
+    
+    var body: some View {
+       MainTabView()
     }
 }
 
