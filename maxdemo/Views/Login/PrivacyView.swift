b7e31cf32bb1202b8a0e37a76d3a49363679cbda
Ryan Helgeson <ryanhelgeson14@gmail.com>
2024-01-20 14:02:09 -0500

    Add a link to privacy and terms

--- maxdemo/Views/Login/PrivacyView.swift
+++ maxdemo/Views/Login/PrivacyView.swift
@@ -7,45 +7,33 @@
 
 import SwiftUI
 
+struct PrivacySettingItem {
+    let id: String = UUID().uuidString
+    let text: String
+    let url: String
+}
+
 struct PrivacyView: View {
 
     var body: some View {
-        let settingsMenuItems: [SettingsMenuItem] = [
-            .init(text: "Privacy Policy", isPush: false, action: {
-                
-            }),
-            .init(text: "Terms of Use", isPush: false, action: {
-                
-            }),
-            .init(text: "Ad Choices", isPush: false, action: {
-                
-            }),
+        let settingsMenuItems: [PrivacySettingItem] = [
+            .init(text: "Privacy Policy", url: "https://www.max.com/privacy?utm_source=vanity_url&utm_campaign=privacy_concent&utm_medium=product&_branch_referrer=H4sIAAAAAAAAA8soKSkottLXz8nMyy7Wy02s0EvOz9VP1S8oyixLTK4EAFnLrMIfAAAA&%24web_only=true&_branch_match_id=1153532637770860486"),
+            .init(text: "Terms of Use", url: "https://www.max.com/terms-of-use?utm_source=vanity_url&utm_campaign=privacy_concent&utm_medium=product&_branch_referrer=H4sIAAAAAAAAA8soKSkottLXz8nMyy7Wy02s0EvOz9VP1S9JLcotBgBjNoSSHQAAAA%3D%3D&%24web_only=true&_branch_match_id=1153532637770860486"),
+            .init(text: "Ad Choices", url: "https://www.warnermediaprivacy.com/opt-out/?utm_source=vanity_url&utm_campaign=privacy_concent&utm_medium=product&_branch_referrer=H4sIAAAAAAAAA8soKSkottLXz8nMyy7Wy02s0EvOz9VP1U9M0U3OyM9MTi0GALGiNM0iAAAA&%24web_only=true&_branch_match_id=1153532637770860486"),
         ]
         VStack {
             ScrollView {
                 VStack(alignment: .leading) {
-                    ForEach(settingsMenuItems, id: \.id) { item in
-                        Button {
-                            debugPrint("Doing \(item.text)")
-                        } label: {
+                    ForEach(settingsMenuItems, id: \.text) { item in
+                        Link(destination: URL(string: item.url)!, label: {
                             HStack {
                                 Text(item.text)
                                     .foregroundStyle(.white)
                                     .font(.system(size: 16, weight: .bold))
                                     .padding(12)
-                                
                                 Spacer()
-                                
-                                if item.isPush {
-                                    Image(systemName: "chevron.right")
-                                        .resizable()
-                                        .scaledToFit()
-                                        .foregroundStyle(.white)
-                                        .frame(width: 20, height: 20)
-                                        .padding(12)
-                                }
                             }
-                        }
+                        })
                         
                         if item.id != settingsMenuItems.last?.id {
                             Rectangle()
